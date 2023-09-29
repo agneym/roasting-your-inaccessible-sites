b1d10b2e453481a35e088670ee06487aa894b41c
Agney Menon <agney@circle.co>
2023-09-29 22:44:57 +0530

    prettier update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from 'react'
+import { useLayoutEffect } from "react";
 import Reveal from "reveal.js";
 import RevealMarkdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
@@ -14,21 +14,25 @@ function App() {
   return (
     <div className="slides">
       <section>
-        <h1>Roasting your <br />Non Accessible <br />Websites</h1>
+        <h1>
+          Roasting your <br />
+          Non Accessible <br />
+          Websites
+        </h1>
       </section>
       <section data-auto-animate>
         <h2>Accessibility</h2>
       </section>
       <section data-auto-animate>
-          <h1>
-            <span>a</span>
-            <span className="text-slate-700">11</span>
-            <span>y</span>
-          </h1>
-          <h2 className="text-gray-800">Accessibility</h2>
-        </section>
+        <h1>
+          <span>a</span>
+          <span className="text-slate-700">11</span>
+          <span>y</span>
+        </h1>
+        <h2 className="text-gray-800">Accessibility</h2>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

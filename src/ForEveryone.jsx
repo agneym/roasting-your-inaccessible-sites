c6b76e3564a7d33b58a13472caa7f52b66e28f52
Agney Menon <agney@circle.co>
2023-10-02 01:55:12 +0530

    restructure for everyone

--- src/ForEveryone.jsx
+++ src/ForEveryone.jsx
@@ -2,27 +2,6 @@ export function ForEveryone() {
   return (
     <>
       <section>
-        <figure>
-          <blockquote className="w-full text-5xl shadow-none [text-wrap:balance]">
-            An estimated <span className="font-bold">1.3 billion people</span>{" "}
-            experience significant disability. This represents{" "}
-            <span className="font-bold">16%</span> of the world&apos;s
-            population, or <span className="font-bold">1 in 6</span> of us.
-          </blockquote>
-          <figcaption>- WHO Factsheet</figcaption>
-        </figure>
-      </section>
-      <section>
-        <figure>
-          <blockquote className="w-full text-5xl shadow-none [text-wrap:balance]">
-            Across the one million home pages, <strong>49,991,225</strong>{" "}
-            distinct accessibility errors were detected—an average of{" "}
-            <strong>50</strong> errors per page
-          </blockquote>
-          <figcaption>- The WebAIM Million 2023</figcaption>
-        </figure>
-      </section>
-      <section>
         <h1 className="r-fit-text">
           <span>Accessibility is</span>
           <br />
@@ -44,11 +23,35 @@ export function ForEveryone() {
         </figure>
       </section>
       <section>
-        <h1 className="max-w-6xl text-6xl leading-normal [text-wrap:balance]">
-          In 2019, Kerala High Court declared that the right to have access to
-          Internet becomes the part of right to education as well as right to
-          privacy under Article 21 of the Constitution of India.
+        <h1 className="max-w-6xl text-5xl font-normal leading-normal text-gray-700 [text-wrap:balance]">
+          In 2019, Kerala High Court declared that the right to have{" "}
+          <span className="font-bold text-gray-800">
+            access to Internet becomes the part of right to education as well as
+            right to privacy
+          </span>{" "}
+          under Article 21 of the Constitution of India.
         </h1>
+      </section>
+      <section>
+        <figure>
+          <blockquote className="w-full text-5xl shadow-none [text-wrap:balance]">
+            An estimated <span className="font-bold">1.3 billion people</span>{" "}
+            experience significant disability. This represents{" "}
+            <span className="font-bold">16%</span> of the world&apos;s
+            population, or <span className="font-bold">1 in 6</span> of us.
+          </blockquote>
+          <figcaption>- WHO Factsheet</figcaption>
+        </figure>
+      </section>
+      <section>
+        <figure>
+          <blockquote className="w-full text-5xl shadow-none [text-wrap:balance]">
+            Across the one million home pages, <strong>49,991,225</strong>{" "}
+            distinct accessibility errors were detected—an average of{" "}
+            <strong>50</strong> errors per page
+          </blockquote>
+          <figcaption>- The WebAIM Million 2023</figcaption>
+        </figure>
       </section>
       <section>
         <table className="max-w-full text-xl">
